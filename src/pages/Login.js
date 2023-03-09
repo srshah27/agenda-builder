d4331af19735bf3af2e827d59f722402a6973f7f
srshah27 <snehilshah27@gmail.com>
2023-03-10 03:14:29 +0530

    trello-login

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,36 +1,43 @@
 import React from 'react'
-import { Formik, Form, Field, ErrorMessage } from 'formik';
-
+import { Formik, Form, Field, ErrorMessage } from 'formik'
+import Image from 'next/image'
 function Login() {
   return (
-    <section>
-      <div>
-        <div>
-          <form action="" method="post">
-            <h2>Login</h2>
-            <div>
-              <input type="email" required />
-              <label htmlFor="">Email</label>
+    <div className="">
+      <Image
+        className="inline mx-auto"
+        src="/Planit.svg"
+        alt="Planit"
+        width={120}
+        height={150}
+      />
+      <span>Agile Builder</span>
+
+      <section className="w-full p-0">
+        <div className="mx-auto my-0 w-auto max-w-lg">
+          <div className="shadow-lg py-[25px] px-[40px]">
+            <h1 className="text-center text-base mt-3 mb-6 tracking-tight">
+              Login to Agile Builder
+            </h1>
+            <div className="flex flex-col items-center ">
+              <form className="block mt-0 w-full px-5">
+                <input
+                  type="text"
+                  placeholder="Type here"
+                  className="input-md bg-slate-300 transition-all duration-500 input-bordered w-full rounded-md"
+                />
+                <input
+                  type="password"
+                  placeholder="Type here"
+                  className="input-md bg-slate-300 transition-all duration-500 input-bordered w-full rounded-md"
+                />
+                <button className="btn btn-active btn-primary rounded-md">Button</button>
+              </form>
             </div>
-            <div>
-              <input type="password" required />
-              <label htmlFor="">Password</label>
-            </div>
-            <div>
-              <label htmlFor="">
-                <input type="checkbox" name="" id="" />
-                Remeber Me
-              </label>
-              <a href="#">Forget Password</a>
-            </div>
-            <button>Login</button>
-            <div className="register">
-              <p>Dont Have an Account? <a href="#">SignUp</a></p>
-            </div>
-          </form>
+          </div>
         </div>
-      </div>
-    </section>
+      </section>
+    </div>
   )
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -12,29 +12,32 @@ module.exports = {
     extend: {},
   },
   plugins: [require('daisyui')],
+  // daisyui: {
+  //   themes: [
+  //     {
+  //       mytheme: {
+  //         primary: '#2563eb',
+
+  //         secondary: '#7B92B2',
+
+  //         accent: '#67CBA0',
+
+  //         neutral: '#3b82f6',
+
+  //         'base-100': '#2A303C',
+
+  //         info: '#3ABFF8',
+
+  //         success: '#36D399',
+
+  //         warning: '#FBBD23',
+
+  //         error: '#F87272',
+  //       },
+  //     },
+  //   ],
+  // },
   daisyui: {
-    themes: [
-      {
-        mytheme: {
-          primary: '#2563eb',
-
-          secondary: '#7B92B2',
-
-          accent: '#67CBA0',
-
-          neutral: '#3b82f6',
-
-          'base-100': '#2A303C',
-
-          info: '#3ABFF8',
-
-          success: '#36D399',
-
-          warning: '#FBBD23',
-
-          error: '#F87272',
-        },
-      },
-    ],
+    themes: ["cupcake"],
   },
 }
