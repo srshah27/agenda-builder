162150e2a1157a2800de13adf6d2793e23b797f3
Alefiya Abbas <alefiyar02@gmail.com>
2023-03-10 21:28:36 +0530

    login updated

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -6,9 +6,9 @@ import Image from 'next/image'
 function Login() {
   return (
     <div className="mt-20">
-      <div className="flex items-center justify-center text-3xl md:text-4xl text-accent font-mono font-bold">
+      <div className="flex items-center justify-center text-3xl md:text-4xl text-accent font-mono font-bold  mx-52">
         <Image
-          className="inline mx-5 my-8 w-10 h-10 md:w-12 md:h-16"
+          className="inline mr-5 my-8 w-10 h-10 md:w-12 md:h-16 "
           src="/logo.svg"
           alt="agenda"
           width={50}
@@ -27,35 +27,34 @@ function Login() {
               <input
                 type="text"
                 placeholder="Enter Email"
-                className="input-md mt-2 transition-all duration-500 w-full h-10 rounded-sm border-gray-400 border-2"
+                className="input-md mt-2 transition-all duration-500 w-full h-10 rounded-sm border-gray-300 border-2 bg-gray-50"
               />
               <input
                 type="password"
                 placeholder="Password"
-                className="input-md my-2 transition-all duration-500 input-bordered w-full h-10 rounded-sm  border-gray-400 border-2"
+                className="input-md my-2 transition-all duration-500 input-bordered w-full h-10 rounded-sm  border-gray-300 border-2  bg-gray-50"
               />
-              <button className="btn-active w-full h-10 rounded-sm font-mono my-2 text-cyan-200">
+              <button className="btn-active w-full h-10 rounded-md font-mono my-2 font-bold text-gray-100 bg-accent">
                 Log In
               </button>
-              <div className="text-center text-gray-400 text-xs my-3"> OR </div>
+              <div className="text-center text-gray-500 text-xs my-3"> OR </div>
               <button className=" justify-center items-center  h-10 w-full text-center shadow-lg border-2 border-gray-50 rounded-sm">
                 <FcGoogle className="inline  w-5 h-auto mx-2 my-2 " />
                 <div className=" inline font-bold text-slate-600">
                   Continue with Google
                 </div>
               </button>
-              <div className="divider my-8 h-1"></div>
+              <div className="divider my-6 h-1"></div>
               <div className="inline mx-10 text-gray-400 text-sm">
-                {' '}
-                Don&apos;t have an account? <RxDotFilled className="inline" />
-                <strong>Sign Up!</strong>
+                Don't have an account? <RxDotFilled className="inline text-gray-800" /> Sign Up!
               </div>
             </form>
           </div>
         </div>
       </section>
       <div>
-        <Image src="/wavesOpacity.svg" alt="Waves" className='scale-y-[-1] absolute w-screen h-1/3 bottom-0 -z-10' width={500} height={500} />
+        <Image src="/" alt="image1" className=' inline absolute w-1/3 h-2/3 bottom-0  left-0 -z-10' width={300} height={200} />
+        <Image src="/" alt="image2" className=' inline absolute w-1/3 h-2/3 bottom-0  right-0 -z-10' width={300} height={200} />
       </div>
     </div>
   )
