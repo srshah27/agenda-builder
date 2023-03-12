e63a0513a37e9b96b84b75573617d8a8532fafe2
srshah27 <snehilshah27@gmail.com>
2023-03-13 03:59:18 +0530

    navbar in progress

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,43 +2,27 @@ import Image from 'next/image'
 
 function Navbar() {
   return (
-    <div className="navbar bg-base-100">
-      <div className="flex-1">
-        <a className="btn btn-ghost normal-case text-xl">daisyUI</a>
-      </div>
-      <div className="flex-none">
-        <ul className="menu menu-horizontal px-1">
-          <li>
-            <a>Item 1</a>
-          </li>
-          <li tabIndex={0}>
-            <a>
-              Parent
-              <svg
-                className="fill-current"
-                xmlns="http://www.w3.org/2000/svg"
-                width="20"
-                height="20"
-                viewBox="0 0 24 24"
-              >
-                <path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z" />
-              </svg>
-            </a>
-            <ul className="p-2 bg-base-100">
-              <li>
-                <a>Submenu 1</a>
-              </li>
-              <li>
-                <a>Submenu 2</a>
-              </li>
-            </ul>
-          </li>
-          <li>
-            <a>Item 3</a>
-          </li>
-        </ul>
-      </div>
-    </div>
+    <header>
+      <nav className="fixed top-0 z-50 w-screen shadow-md">
+        <div className="flex items-stretch mx-auto my-0">
+          <a href="/" className="flex items-center p-4">
+            <Image
+              className="i"
+              src="/logo.svg"
+              alt="agenda"
+              width={50}
+              height={63}
+            />
+            <h1 className="ml-4">Agenda Builder</h1>
+          </a>
+          <div className='flex'>
+            <button>
+              
+            </button>
+          </div>
+        </div>
+      </nav>
+    </header>
   )
 }
 
