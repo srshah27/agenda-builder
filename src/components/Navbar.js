6c6f5c8044daad091769ecf922df79a1030d6bc5
srshah27 <snehilshah27@gmail.com>
2023-03-13 14:40:28 +0530

    corrected link

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,24 +1,25 @@
 import Image from 'next/image'
+import Link from 'next/link'
 
 function Navbar() {
   return (
     <header>
       <nav className="fixed top-0 z-50 w-screen shadow-md">
         <div className="flex items-stretch mx-auto my-0">
-          <a href="/" className="flex items-center p-4">
-            <Image
-              className="i"
-              src="/logo.svg"
-              alt="agenda"
-              width={50}
-              height={63}
-            />
-            <h1 className="ml-4">Agenda Builder</h1>
-          </a>
-          <div className='flex'>
-            <button>
-              
-            </button>
+          <Link href="/">
+            <a className="flex items-center p-4">
+              <Image
+                className="i"
+                src="/logo.svg"
+                alt="agenda"
+                width={50}
+                height={63}
+              />
+              <h1 className="ml-4">Agenda Builder</h1>
+            </a>
+          </Link>
+          <div className="flex">
+            <button></button>
           </div>
         </div>
       </nav>
