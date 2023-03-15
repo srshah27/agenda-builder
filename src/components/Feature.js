66b7d99089c3c740294abb531d16f9a389d9f00e
srshah27 <snehilshah27@gmail.com>
2023-03-15 22:48:28 +0530

    removed apostrophy

--- src/components/Feature.js
+++ src/components/Feature.js
@@ -18,8 +18,8 @@ const Feature = () => {
             <div className ="text-2xl text-primary text-left space-x-6">
                 <h1 className="text-3xl font-semibold mb-6">Features:</h1>
                 
-                <li className="mb-2"> To design an AI based system that  manages the user's<br/> time according to priority of tasks.</li>
-                <li className="mb-2"> To improve overall efficiency by organizing the workload<br/> in a balanced manner and tracking user's habits.</li>
+                <li className="mb-2"> To design an AI based system that  manages the users<br/> time according to priority of tasks.</li>
+                <li className="mb-2"> To improve overall efficiency by organizing the workload<br/> in a balanced manner and tracking users habits.</li>
                 <li className="mb-2"> To provide timely remainder for upcoming deadlines and <br/>scheduled tasks.</li>
                 <li className="mb-2"> To promote cross referencing between  different scheduling <br/> platforms.</li>
             </div>
