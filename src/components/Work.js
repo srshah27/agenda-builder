9cbd117d791cd49a9e374a696cdb20134a735a1f
Alefiya Abbas <alefiyar02@gmail.com>
2023-03-21 15:33:49 +0530

    margin adjusted

--- src/components/Work.js
+++ src/components/Work.js
@@ -33,7 +33,7 @@ const Workspace = ({ id, workspace }) => {
 const Work = ({ asCreator, asCollaborator }) => {
   console.log(asCreator.length)
   return (
-      <Stack left={["0%", "0%", "16%", "16%"]} m="4" pos="fixed">
+      <Stack m="12" pos="fixed">
         {asCreator.length > 0 && <Text fontSize="lg" color="red" > Workspaces you created</Text>}
         {asCreator.map((workspace) => (<Workspace key={workspace.id} id={workspace.id} workspace={workspace} />))}
 
@@ -44,32 +44,3 @@ const Work = ({ asCreator, asCollaborator }) => {
 }
 
 export default Work
-
-  // < Text fontSize = "lg" color = "red" > Your Workspaces</ >
-  //   <Flex >
-  //     <Stack>
-  //       <Text fontSize="sm" color="gray.500"> Your workspaces are where you can organize your projects and tasks. </Text>
-  //       <Wrap>
-  //         <WrapItem>
-  //           <Center w='180px' h='80px' bg='red.200'>
-  //             Box 1
-  //           </Center>
-  //         </WrapItem>
-  //         <WrapItem>
-  //           <Center w='180px' h='80px' bg='green.200'>
-  //             Box 2
-  //           </Center>
-  //         </WrapItem>
-  //         <WrapItem>
-  //           <Center w='180px' h='80px' bg='tomato'>
-  //             Box 3
-  //           </Center>
-  //         </WrapItem>
-  //         <WrapItem>
-  //           <Center w='180px' h='80px' bg='blue.200'>
-  //             Box 4
-  //           </Center>
-  //         </WrapItem>
-  //       </Wrap>
-  //     </Stack>
-  //   </Flex>
