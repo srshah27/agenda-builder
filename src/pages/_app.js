f356c237115f0e636b992786baccfeeaa7c710ad
srshah27 <snehilshah27@gmail.com>
2023-03-23 00:40:49 +0530

    configured progressbar

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -2,11 +2,18 @@ import '@/styles/globals.css'
 import { SessionProvider } from 'next-auth/react'
 import store from '@/redux/store'
 import { Provider } from 'react-redux'
+import NextNProgress from 'nextjs-progressbar'
 
 export default function App({ Component, pageProps, session }) {
   return (
     <Provider store={store}>
       <SessionProvider session={session}>
+        <NextNProgress
+          color="#0079bf"
+          startPosition={0.3}
+          stopDelayMs={200}
+          height={4}
+        />
         <Component {...pageProps} />
       </SessionProvider>
     </Provider>
