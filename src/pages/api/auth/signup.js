1f704542bf18e0dd581895ce5cc963fe81dd20c1
AdityaSawant0912 <arsawant09@gmail.com>
2023-03-25 15:48:36 +0530

    signup changes

--- src/pages/api/auth/signup.js
+++ src/pages/api/auth/signup.js
@@ -1,6 +1,7 @@
 import dbConnect from '@/lib/dbconnect'
 import User from '@/models/Users'
 import { hash } from 'bcryptjs'
+import { nanoid } from 'nanoid'
 
 export default async function handler(req, res) {
   if (req.method !== 'POST') return res.status(405).end()
@@ -13,8 +14,15 @@ export default async function handler(req, res) {
   if (duplicate) return res.status(409).json({ error: 'User already exists' })
   const hashed = await hash(password, 11)
   console.log(hashed)
+  const query = { uid: new RegExp(`^${name.replace(/[^a-zA-Z]/g, '').toLowerCase()}`, 'i') };
+  const docs = await User.find(query);
+  let uid = name.replace(/[^a-zA-Z]/g, '').toLowerCase();
+  if (docs.length > 0) {
+    uid += docs.length;
+  }
+  
   try {
-    let user = await User.create({ name, email, password: hashed })
+    let user = await User.create({ name, email, password: hashed, uid: uid || nanoid(10) })
     console.log(user)
     res.status(201).json({ user })
   } catch (error) {
