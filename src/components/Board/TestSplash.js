523de860259e5b65c42d34963cd559e305fdac7c
srshah27 <snehilshah27@gmail.com>
2023-03-26 00:00:55 +0530

    unsplash api

---
+++ src/components/Board/TestSplash.js
@@ -0,0 +1,14 @@
+import UnsplashReact, { Base64Uploader, withDefaultProps } from "unsplash-react"
+
+const MY_ACCESS_KEY = "Agenda Builder"
+
+const TestSplash = () => {
+  return (
+    <UnsplashReact
+      accessKey={MY_ACCESS_KEY}
+      Uploader={withDefaultProps(Base64Uploader, { name: 'event[logo]' })}
+    />
+  )
+}
+
+export default TestSplash
