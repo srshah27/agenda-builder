d3fd70cc97f7c4d8ecbbfc19d71f42cd8d167265
srshah27 <snehilshah27@gmail.com>
2023-03-27 00:25:14 +0530

    changes in UI

--- src/components/Board/List.js
+++ src/components/Board/List.js
@@ -4,16 +4,16 @@ import TaskList from './TaskList'
 import { useColorModeValue, Box } from '@chakra-ui/react'
 
 const List = ({ list, tasks, index }) => {
-  let _color = useColorModeValue('gray.50', 'gray.700')
+  let _color = useColorModeValue('gray.100', 'gray.700')
   // let textColor = useColorModeValue('gray.700', 'gray.50')
   return (
     <Draggable draggableId={list.id} index={index}>
       {(draggableProvided, draggableSnapshot) => (
-        <Box bgColor={_color}
+        <Box
+          bgColor={_color}
           {...draggableProvided.draggableProps}
           ref={draggableProvided.innerRef}
-          className={`flex flex-col w-[250px] m-4 border rounded shadow-md 
-            }`}
+          className={`flex flex-col w-[280px] m-4 rounded shadow-md`}
         >
           <Box {...draggableProvided.dragHandleProps} className="p-2 text-md">
             {list.name}
@@ -23,8 +23,7 @@ const List = ({ list, tasks, index }) => {
               <Box
                 ref={droppableProvided.innerRef}
                 {...droppableProvided.droppableProps}
-                className={`grow min-h-[100px] p-2 border-t-2
-                  }`}
+                className={`min-h-[100px] p-2 border-t-2`}
               >
                 <TaskList tasks={tasks} list={list} />
 
--- src/components/Board/TaskList.js
+++ src/components/Board/TaskList.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import { Draggable } from 'react-beautiful-dnd'
 import Task from './Task'
 
 const TaskList = ({ tasks, list }) => {
