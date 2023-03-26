38fed592707218a8eeea9df238d9d2aeee4b0ab2
Alefiya Abbas <alefiyar02@gmail.com>
2023-03-26 22:00:34 +0530

    bored of board

--- src/components/Board/Task.js
+++ src/components/Board/Task.js
@@ -1,20 +1,22 @@
 import React from 'react'
 import { Draggable } from 'react-beautiful-dnd'
+import { Box, useColorModeValue } from '@chakra-ui/react'
 
 const Task = ({ task, index }) => {
+  let _color = useColorModeValue('gray.50', 'gray.900')
   return (
     <Draggable draggableId={task.id} index={index}>
       {(draggableProvided, draggableSnapshot) => (
-        <div
+        <Box bgColor={_color}
           {...draggableProvided.draggableProps}
           {...draggableProvided.dragHandleProps}
           ref={draggableProvided.innerRef}
-          className={`flex p-2 mb-2 border rounded-md w-full ${
-            draggableSnapshot.isDragging ? 'bg-emerald-300' : 'bg-emerald-800'
+          className={`flex p-2 mb-2 border rounded-md w-full shadow-md ${
+            draggableSnapshot.isDragging ? 'bg-gray-500' : 'bg-gray-900'
           } `}
         >
           {task.name}
-        </div>
+        </Box>
       )}
     </Draggable>
   )
