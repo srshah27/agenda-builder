bf593dd63284b9bd6ef94960ddd17ba1f419f2fc
srshah27 <snehilshah27@gmail.com>
2023-04-10 02:23:17 +0530

    added font

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -2,19 +2,28 @@ import '@/styles/globals.css'
 import { SessionProvider } from 'next-auth/react'
 import NextNProgress from 'nextjs-progressbar'
 import { ChakraProvider } from '@chakra-ui/react'
+import localFont from '@next/font/local'
+import Head from 'next/head'
+
+const myFont = localFont({ src: '../../public/Fonts/GeneralSans_Complete/GeneralSans_Complete/Fonts/Variable/GeneralSans-Variable.ttf' })
 
 export default function App({ Component, pageProps, session }) {
+  <Head>
+    <link href="https://api.fontshare.com/v2/css?f[]=general-sans@500,600,400,700&display=swap" rel="stylesheet"></link>
+  </Head>
   return (
     // <ChakraProvider>
-      <SessionProvider session={session}>
-        <NextNProgress
-          color="#0079bf"
-          startPosition={0.3}
-          stopDelayMs={200}
-          height={4}
-        />
+    <SessionProvider session={session}>
+      <NextNProgress
+        color="#0079bf"
+        startPosition={0.3}
+        stopDelayMs={200}
+        height={4}
+      />
+      <main className='myFont.className'>
         <Component {...pageProps} />
-      </SessionProvider>
+      </main>
+    </SessionProvider>
     // </ChakraProvider>
   )
 }
