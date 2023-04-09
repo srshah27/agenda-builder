6cf90b2f5e0239e60626985d6c5b706c46224146
srshah27 <snehilshah27@gmail.com>
2023-04-09 17:17:30 +0530

    card modal

---
+++ src/components/Modals/CardModal.js
@@ -0,0 +1,61 @@
+import React from 'react'
+import {
+  Modal, ModalOverlay, ModalContent, ModalHeader, ModalFooter,
+  ModalBody, ModalCloseButton,
+  Button, FormControl, FormLabel,
+  Input, Textarea,
+  useDisclosure
+} from '@chakra-ui/react'
+
+
+
+const CardModal = () => {
+  const { isOpen, onOpen, onClose } = useDisclosure()
+  const initialRef = React.useRef(null)
+  return (
+    <>
+      <Button onClick={onOpen}>Open Modal</Button>
+
+      <Modal
+        initialFocusRef={initialRef}
+        isOpen={isOpen}
+        onClose={onClose}
+        blockScrollOnMount={false}
+        isCentered
+        motionPreset='slideInBottom'
+        size={'xl'}
+      >
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>Edit Card</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody pb={6}>
+            <FormControl>
+              <FormLabel>Card Name</FormLabel>
+              <Input ref={initialRef} placeholder='Snehil' />
+            </FormControl>
+
+            <FormControl mt={4}>
+              <FormLabel>Description</FormLabel>
+              <Textarea placeholder='Here is a sample placeholder' />
+            </FormControl>
+
+            {/* <FormControl mt={4}>
+              <FormLabel>Assigned To</FormLabel>
+              <Input ref={initialRef} placeholder='Snehil' />
+            </FormControl> */}
+          </ModalBody>
+
+          <ModalFooter>
+            <Button colorScheme='blue' mr={3}>
+              Save
+            </Button>
+            <Button onClick={onClose}>Cancel</Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+    </>
+  )
+}
+
+export default CardModal
