42bbfab3584905edd241ec4adce7048333ba07d2
srshah27 <snehilshah27@gmail.com>
2023-06-15 03:08:29 +0530

    list in progress

--- src/components/Board/List.js
+++ src/components/Board/List.js
@@ -4,9 +4,8 @@ import TaskList from './TaskList'
 import { Box, Flex, Text } from '@chakra-ui/react'
 import { ContextMenu, MenuItem, ContextMenuTrigger } from 'react-contextmenu'
 import { AddIcon } from '@chakra-ui/icons'
-import moment from 'moment';
+import moment from 'moment'
 import CustomInput from '../utils/CustomInput'
-
 
 const List = ({ list, tasks, index, addCard, deleteListOrCard }) => {
   const [listName, setListName] = useState(list.name)
@@ -21,42 +20,50 @@ const List = ({ list, tasks, index, addCard, deleteListOrCard }) => {
     let data = await res.json()
   }
   // const [start, setstart] = useState(`${new Date(list.start).getHours()}:${new Date(list.start).getMinutes()}`);
-  const [start, setStart] = useState(moment(list.start).format('HH:mm'));
-  const [end, setEnd] = useState(moment(list.end).format('HH:mm'));
+  const [start, setStart] = useState(moment(list.start).format('HH:mm'))
+  const [end, setEnd] = useState(moment(list.end).format('HH:mm'))
   return (
     <Draggable draggableId={list.id} index={index}>
       {(draggableProvided, draggableSnapshot) => (
-
         <div
           {...draggableProvided.draggableProps}
           ref={draggableProvided.innerRef}
-          className={`m-4 border rounded shadow-md bg-red-400 w-4/5 h-fit flex flex-col`}
+          className={`m-4 rounded-xl card shadow-md w-4/5 h-fit flex flex-col justify-center items-center`}
         >
           <ContextMenuTrigger key={list.id} id={list.id}>
-            <div {...draggableProvided.dragHandleProps} className="p-2 flex justify-center">
+            <div
+              {...draggableProvided.dragHandleProps}
+              className="p-2 flex justify-center"
+            >
               {/* <input value={listName} className='text-center' onChange={handleListName} /> */}
-              <CustomInput center={true} placeholder='Title' />
+              <CustomInput center={true} placeholder="Title" />
 
               {/* <Spacer /> */}
               {/* <input type='time' value={start} min={'09:00:00'} max={'20:00:00'} onChange={(e) => setStart(e.target.value)} />
               <input type='time' value={end} onChange={(e) => setEnd(e.target.value)} /> */}
-
             </div>
           </ContextMenuTrigger>
 
+          <TaskList
+            tasks={tasks}
+            list={list}
+            deleteListOrCard={deleteListOrCard}
+          />
+          <button
+            className={`flex rounded-md p-2 m-2 justify-center items-center glass`}
+            onClick={() => addCard(list.id)}
+          >
+            <AddIcon w={3} h={3} mr={3} />
+            Add a card
+          </button>
 
-          <TaskList tasks={tasks} list={list} deleteListOrCard={deleteListOrCard} />
-          <Box className='px-2'>
-            <Box
-              className={`flex p-2 mb-2 w-full shadow-md bg-indigo-200`}
-              as='button'
-              alignItems={'center'}
-              onClick={() => addCard(list.id)}
-            >
+          {/* <button class="cssbuttons-io-button">
+            {' '}
+            Get started
+            <div class="icon">
               <AddIcon w={3} h={3} mr={3} />
-              Add a card
-            </Box>
-          </Box>
+            </div>
+          </button> */}
           <ContextMenu id={list.id}>
             <Box m={2} bg="gray.100" w={130} rounded={5}>
               <MenuItem
@@ -70,7 +77,6 @@ const List = ({ list, tasks, index, addCard, deleteListOrCard }) => {
             </Box>
           </ContextMenu>
         </div>
-
       )}
     </Draggable>
   )
--- src/components/Board/TaskList.js
+++ src/components/Board/TaskList.js
@@ -8,9 +8,9 @@ const TaskList = ({ tasks, list, deleteListOrCard }) => {
         <div
           ref={droppableProvided.innerRef}
           {...droppableProvided.droppableProps}
-          className={`p-2 w-full flex flex-col min-h-[150px} bg-green-400`}
+          className={`w-full flex flex-col min-h-fit`}
         >
-          <div className="flex flex-col mt-8">
+          <div className="flex flex-col">
             {tasks.map((task, index) => (
               <Task task={task} index={task.sequence} key={task.index} />
             ))}
